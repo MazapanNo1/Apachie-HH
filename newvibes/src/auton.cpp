a976a06d47cfc671d5b450c3495024f7f30336f8
ll-Art-ll <45616747+ll-Art-ll@users.noreply.github.com>
2020-02-13 09:15:24 -0800

    Add files via upload

--- newvibes/src/auton.cpp
+++ newvibes/src/auton.cpp
@@ -6,4 +6,22 @@ right_wheel.move_velocity(right);
 left_wheel.move_velocity(left);
 pros::delay(time);
 
+
+
+void oneCubeUnit(){
+  right_wheel.move_velocity(70);
+  left_wheel.move_velocity(70);
+  pros::delay(500);
+
+}
+
+int coordinatnt(double x, double y){
+
+
+}
+
+void movement(){
+
+
+}
 }
